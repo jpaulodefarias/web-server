96d906a2f6acd7cb14ddda764bf4e8161dd48532
João Paulo de Farias <jpaulodefarias@gmail.com>
2021-03-15 01:25:55 -0300

    add multithreading

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,22 @@ use std::{
     fs,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use web_server::thread_pool::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -19,8 +26,12 @@ fn handle_connection(mut stream: TcpStream) -> () {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "pages/hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "pages/hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "pages/404.html")
